c8f3f5c9405342a5072b0b254944334ab053d0a4
mDanielSE <daniel.marques@edu.unifil.br>
2022-10-15 16:33:07 -0300

    entity Client

---
+++ src/model/entities/Client.java
@@ -0,0 +1,44 @@
+package model.entities;
+
+public class Client {
+
+    private Integer id;
+    private String name;
+    private String email;
+    private Order order;
+
+    public Client(Integer id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void createOrder(Order order) {
+        this.order = order;
+    }
+
+}
