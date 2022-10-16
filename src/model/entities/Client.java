d88afbe39231843b2da45c0f821e67dcba73a4a5
mDanielSE <daniel.marques@edu.unifil.br>
2022-10-16 09:31:54 -0300

    toString Client

--- src/model/entities/Client.java
+++ src/model/entities/Client.java
@@ -38,4 +38,10 @@ public class Client {
         getOrderList().add(order);
     }
 
+    @Override
+    public String toString(){
+        return "Name: " + this.name
+                + ", Email: " + this.email;
+    }
+
 }
